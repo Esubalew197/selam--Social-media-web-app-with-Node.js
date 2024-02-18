0271dfa35d935d58387d6b468dcbdff5c7a376e3
Esubalew197 <Esubalewmillion@gmail.com>
2024-02-18 20:12:52 +0300

    routes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -26,15 +28,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-
-app.get("/", (req,res)=>{
-  res.send("Welcome to homepage")
-})
-
-app.get("/users", (req,res)=>{
-  res.send("Welcome to users page")
-})
-
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
     console.log("BE Server is running!");
